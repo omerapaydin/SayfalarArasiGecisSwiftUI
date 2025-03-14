20c5597850f70f96da0b7756b07f08ff39161fda
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 22:20:36 +0300

    Initial Commit

---
+++ SayfalarArasiGecisSwiftUI/SayfalarArasiGecisSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayfalarArasiGecisSwiftUIApp.swift
+//  SayfalarArasiGecisSwiftUI
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SayfalarArasiGecisSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SayfalarArasiGecisSwiftUITests/SayfalarArasiGecisSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SayfalarArasiGecisSwiftUITests.swift
+//  SayfalarArasiGecisSwiftUITests
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import Testing
+@testable import SayfalarArasiGecisSwiftUI
+
+struct SayfalarArasiGecisSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
