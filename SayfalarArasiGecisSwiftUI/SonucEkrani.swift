da2334161d3085dada21678ad56c8a20b3daa61c
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 22:37:41 +0300

    dismiss

---
+++ SayfalarArasiGecisSwiftUI/SonucEkrani.swift
@@ -0,0 +1,18 @@
+//
+//  SonucEkrani.swift
+//  SayfalarArasiGecisSwiftUI
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import SwiftUI
+
+struct SonucEkrani: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SonucEkrani()
+}
